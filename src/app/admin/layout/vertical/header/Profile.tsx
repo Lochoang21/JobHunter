526b4e8191c5e2fc315900928579ea98b52d3094
LocHoang1 <lochoang2101@gmail.com>
2025-07-21 15:12:05 +0700

    update

--- src/app/admin/layout/vertical/header/Profile.tsx
+++ src/app/admin/layout/vertical/header/Profile.tsx
@@ -26,27 +26,11 @@ const Profile = () => {
 
         <Dropdown.Item
           as={Link}
-          href="#"
+         href="/profile"
           className="px-3 py-3 flex items-center bg-hover group/link w-full gap-3 text-dark"
         >
           <Icon icon="solar:user-circle-outline" height={20} />
           My Profile
-        </Dropdown.Item>
-        <Dropdown.Item
-          as={Link}
-          href="#"
-          className="px-3 py-3 flex items-center bg-hover group/link w-full gap-3 text-dark"
-        >
-          <Icon icon="solar:letter-linear" height={20} />
-          My Account
-        </Dropdown.Item>
-        <Dropdown.Item
-          as={Link}
-          href="#"
-          className="px-3 py-3 flex items-center bg-hover group/link w-full gap-3 text-dark"
-        >
-          <Icon icon="solar:checklist-linear" height={20} />
-          My Task
         </Dropdown.Item>
         <div className="p-3 pt-0">
         <Button as={Link}  size={'sm'}  href="/auth/login" className="mt-2 border border-primary text-primary bg-transparent hover:bg-lightprimary outline-none focus:outline-none">Logout</Button>
